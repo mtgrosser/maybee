8c42ebc77326e791814957a82792d0a1180123ed
Matthias Grosser <mtgrosser@gmx.net>
2012-06-15 00:00:42 +0200

    Change cruise config

--- cruise_config.rb
+++ cruise_config.rb
@@ -1,8 +1,8 @@
 # Project-specific configuration for CruiseControl.rb
 
 Project.configure do |project|
-  project.use_bundler = false
-  project.build_command = '/usr/local/bin/rbfu @1.9.3 ruby cruise_script.rb'
+  #project.use_bundler = false
+  #project.build_command = '/usr/local/bin/rbfu @1.9.3 ruby cruise_script.rb'
   project.scheduler.polling_interval = 15.minutes
   # Set any args for bundler here
   # Defaults to '--path=#{project.gem_install_path} --gemfile=#{project.gemfile} --no-color'
