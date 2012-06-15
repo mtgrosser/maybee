038b5168c33e5e672d84407cdda8f7857a230267
Matthias Grosser <mtgrosser@gmx.net>
2012-06-15 20:37:25 +0300

    Fixing Make lookup

--- test/models/make.rb
+++ test/models/make.rb
@@ -3,12 +3,11 @@ class Make < ActiveRecord::Base
   has_and_belongs_to_many :workshops
 
   validates_presence_of :name
-  
+
   class << self
-    def [](*args)
-      args.flatten!
-      raise ArgumentError if args.empty?
-      args.map { |name| find_by_name!(name.to_s) }
+    def [](name)
+      raise ArgumentError if name.blank?
+      find_by_name!(name.to_s)
     end
   end
 end
