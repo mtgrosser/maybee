fea80e79cdeaa33d01895c9515c97ad6ad80f26f
Matthias Grosser <mtgrosser@gmx.net>
2012-06-15 15:41:56 +0300

    Add methods for authorization objects

--- lib/maybee/authorization_subject.rb
+++ lib/maybee/authorization_subject.rb
@@ -3,7 +3,11 @@ module Maybee
   module AuthorizationSubject
     
     def may?(access, object)
+      object.allow?(access, self)
+    end
     
+    def authorized_to?(access, object)
+      object.authorize?(access, self)
     end
 
   end
